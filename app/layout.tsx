e002bc99754a80dca10ed1b837d22ab64af814ca
Tyler Lutz <66930763+tyler-lutz@users.noreply.github.com>
2023-06-30 15:07:12 -0500

    Add home page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="mx-auto min-h-screen max-w-2xl bg-slate-900 px-6 py-12 text-slate-400 lg:max-w-5xl">
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,74 @@
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
-  return <main></main>
+  return (
+    <div>
+      <header>
+        <h1 className="text-4xl font-bold tracking-tight text-slate-200">
+          <Link href="/">Tyler Lutz</Link>
+        </h1>
+        <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200">
+          Software Engineer
+        </h2>
+        {/* Social Links */}
+        <div className="mt-8">
+          <div className="flex space-x-4">
+            <a
+              className="hover:text-slate-200"
+              href="https://github.com/tyler-lutz"
+            >
+              <span className="sr-only">GitHub</span>
+              <svg
+                fill="currentColor"
+                viewBox="0 0 16 16"
+                height="24"
+                width="24"
+              >
+                <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z" />
+              </svg>
+            </a>
+            <a
+              className="hover:text-slate-200"
+              href="https://twitter.com/_tylerlutz"
+            >
+              <span className="sr-only">Twitter</span>
+              <svg
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                height="24"
+                width="24"
+              >
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+              </svg>
+            </a>
+            <a
+              className="hover:text-slate-200"
+              href="https://linkedin.com/in/lutz-tyler"
+            >
+              <span className="sr-only">LinkedIn</span>
+              <svg
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                height="24"
+                width="24"
+              >
+                <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z" />
+              </svg>
+            </a>
+          </div>
+        </div>
+        {/* End Social Links */}
+        <div className="mt-8">
+          <p className="mt-4">
+            Hey, I&apos;m Tyler. I&apos;m a software engineer with a passion for
+            front end development.
+          </p>
+          <p className="mt-4">
+            This website is a work in progress. Check back soon for more
+            content!
+          </p>
+        </div>
+      </header>
+    </div>
+  )
 }
